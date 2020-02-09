94951aadf02a63d0a2febd1ff3aa0e9e6e31f60b
Michael Masas <hot.delek@gmail.com>
2020-02-09 22:54:03 +0000

    v.0.2

--- chartmuseum_setup.sh
+++ chartmuseum_setup.sh
@@ -3,7 +3,7 @@ sudo git clone http://github.com/Michael-Masas/todo;
 sudo helm repo add stable 'https://kubernetes-charts.storage.googleapis.com';
 sudo helm repo update;
 sudo helm plugin install https://github.com/chartmuseum/helm-push;
-sudo helm install chartmuseum stable/chartmuseum -f chartmuesum_values.yaml;
+sudo helm install chartmuseum stable/chartmuseum -f todo/chartmuseum_values.yaml;
 sudo helm repo add chartmuseum http://$(sudo minikube ip):33785;
 sudo helm repo update;
 sudo helm package ./todo;
